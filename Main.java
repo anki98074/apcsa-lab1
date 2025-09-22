ac0bf37a3c05d6e5f7b43747a92d2f94a15653c5
Ankita Chowdhary <ankita.chowdhary@gmail.com>
2025-09-22 12:23:10 -0700

    Add Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("AP CSA Lab 01: Hello, VS Code + GitHub!");
+        // TODO: Your lab code here
+    }
+    
+}
